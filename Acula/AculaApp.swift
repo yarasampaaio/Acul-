e0fcad7232e2f1aedc39299986a72d86e5f01e1c
yarasampaaio <yaravitoria16@gmail.com>
2022-05-12 08:26:32 -0300

    Initial Commit

---
+++ Acula/AculaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AculaApp.swift
+//  Acula
+//
+//  Created by Yara Vitoria on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AculaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
